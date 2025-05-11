d9a397ba502c20da747d662cc07002736c16ed48
achref <achref.zammeli@esprit.tn>
2025-05-11 13:27:47 +0100

    Build ready for Azure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   apiUrl: 'https://votre-api-production.com/api',
+    backend_url: 'http://localhost:8081/api/',
+    UserMs: 'http://localhost:8081/api/',
   stripe: {
     publicKey: 'pk_live_votre_clé_publique_stripe_production'
   }
